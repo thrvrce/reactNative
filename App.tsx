57b5fd181422d57e72403683a307135fdd181ee2
thrvrce <70852808+thrvrce@users.noreply.github.com>
2022-01-09 11:30:36 +0300

    Revert "Task3"

--- App.tsx
+++ App.tsx
@@ -1,113 +1,116 @@
-import React, {useState, useEffect, useMemo} from 'react';
-import {View, SafeAreaView, StyleSheet, Text} from 'react-native';
-import {MainScreen} from './react-native-src/pages/Main/Main';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
 import {
-  ProductDetails,
-  IProductOptions,
-} from './react-native-src/pages/ProductDetails/ProductDetails';
-import {IProduct} from './react-native-src/pages/Main/components/Product';
-import {AppContext} from './react-native-src/Context/AppContext';
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+console.log('React native Flipper integration check');
+const Section: React.FC<{
+  title: string;
+}> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
 
 const App = () => {
-  const [isProductsDataLoading, setProductsDataLoadingStatus] = useState(true);
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [options, setOptions] = useState<IProductOptions>({colors: []});
-  const [selectedProductToDisplay, setSelectedProductToDisplay] = useState<
-    null | string
-  >(null);
+  const isDarkMode = useColorScheme() === 'dark';
 
-  const getProducts = async () => {
-    try {
-      const rawResponse = await fetch(
-        'https://rn-mentoring.herokuapp.com/api/v2/storefront/products',
-      );
-      const response = await rawResponse.json();
-      const productsData: IProduct[] = response.data.map(
-        (productDataFromResponse: any) => {
-          const {id, attributes} = productDataFromResponse;
-          const {name, description, price, compare_at_price} = attributes;
-
-          return {
-            id,
-            name,
-            description,
-            imgSrc: 'https://picsum.photos',
-            price: Number(price),
-            // price: Math.round(Number(price) * Math.random()), // uncomment for refresh testing
-            compareAtPrice: Number(compare_at_price ?? price),
-          };
-        },
-      );
-      const optionsData: IProductOptions = {
-        colors: [],
-      };
-
-      optionsData.colors = response.meta.filters.option_types
-        .find((optionType: {name: string}) => optionType.name === 'color')
-        .option_values.map(
-          ({name, presentation}: {name: string; presentation: string}) => ({
-            name,
-            presentation,
-          }),
-        );
-      setProducts(productsData);
-      setOptions(optionsData);
-    } catch (err) {
-      const isError = (err: any): err is Error => {
-        if (err.message) {
-          return true;
-        }
-        return false;
-      };
-
-      if (isError(err)) {
-        console.error(JSON.stringify(err.message));
-      }
-    } finally {
-      setProductsDataLoadingStatus(false);
-    }
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const appContext = useMemo(
-    () => ({
-      loadProductsData: () => {
-        setProductsDataLoadingStatus(true);
-        getProducts();
-      },
-      setSelectedProductToDisplay,
-      isProductsDataLoading,
-    }),
-    [isProductsDataLoading],
-  );
-
-  const selectedProduct = useMemo(
-    () =>
-      selectedProductToDisplay
-        ? products.find(product => product.id === selectedProductToDisplay)
-        : null,
-    [products, selectedProductToDisplay],
-  );
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
   return (
-    <SafeAreaView>
-      <View style={styles.appWrapper}>
-        <AppContext.Provider value={appContext}>
-          {selectedProduct ? (
-            <ProductDetails {...selectedProduct} options={options} />
-          ) : (
-            <MainScreen products={products} />
-          )}
-        </AppContext.Provider>
-      </View>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Hellooooooo! Edit <Text style={styles.highlight}>App.tsx</Text> to
+            change this screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default App;
 const styles = StyleSheet.create({
-  appWrapper: {backgroundColor: '#FFF', height: '100%'},
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
 });
+
+export default App;
