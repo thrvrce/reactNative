b4ee517b6891b10ecf9c049ad6a838870a1cf465
thrvrce <70852808+thrvrce@users.noreply.github.com>
2022-01-09 11:33:06 +0300

    Revert "Revert "Task3""

---
+++ react-native-src/pages/main/components/ProductsList.tsx
@@ -0,0 +1,37 @@
+import React, {FC, useContext} from 'react';
+import {SafeAreaView, StyleSheet, RefreshControl, FlatList} from 'react-native';
+import {Product, IProduct} from './Product';
+import {AppContext} from '../../../Context/AppContext';
+interface IProductsListProps {
+  products: IProduct[];
+}
+
+export const ProductsList: FC<IProductsListProps> = props => {
+  const {products} = props;
+  const {isProductsDataLoading, loadProductsData} = useContext(AppContext);
+  const renderItem = ({item: product}: {item: IProduct}) => (
+    <Product {...product} />
+  );
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        contentContainerStyle={styles.contentContainerWrapper}
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        numColumns={2}
+        refreshControl={
+          <RefreshControl
+            refreshing={isProductsDataLoading}
+            onRefresh={loadProductsData}
+          />
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  contentContainerWrapper: {alignItems: 'center', backgroundColor: '#FFF'},
+});
---
+++ react-native-src/reusabeStyles/textStyles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const textStyles = StyleSheet.create({
+  commonText: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 15,
+    color: '#4A4A4A',
+    marginRight: 10,
+  },
+});
