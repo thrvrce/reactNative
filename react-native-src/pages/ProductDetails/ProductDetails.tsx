8d01676ea92e9f975f4fc8b8301825f876d2ccb2
Victor Avdeev <avdeevvictor95@gmail.com>
2022-01-13 10:14:37 +0300

    fix: move common screen options to parent component screenOptions parameter

--- react-native-src/pages/ProductDetails/ProductDetails.tsx
+++ react-native-src/pages/ProductDetails/ProductDetails.tsx
@@ -32,21 +32,16 @@ export const ProductDetails: FC<ProductDetailsRouteProps> = props => {
             ),
           }}
         />
-        <RootStack.Group screenOptions={{presentation: 'modal'}}>
-          <RootStack.Screen
-            name="ErrorModal"
-            component={CustomErrorModal}
-            options={{headerShown: false}}
-          />
+        <RootStack.Group
+          screenOptions={{presentation: 'modal', headerShown: false}}>
+          <RootStack.Screen name="ErrorModal" component={CustomErrorModal} />
           <RootStack.Screen
             name="SuccessModal"
             component={CustomSuccessModal}
-            options={{headerShown: false}}
           />
           <RootStack.Screen
             name="WarningModal"
             component={CustomWarningModal}
-            options={{headerShown: false}}
           />
         </RootStack.Group>
       </RootStack.Navigator>
